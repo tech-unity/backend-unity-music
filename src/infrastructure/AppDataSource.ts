0dc3feea2f78128e2875deb64cf19c3ce1bf344e
enzomoraes <enzo@DESKTOP-0U8S1NC.localdomain>
2022-06-03 09:04:14 -0400

    chore: adding dotenv

--- src/infrastructure/AppDataSource.ts
+++ src/infrastructure/AppDataSource.ts
@@ -3,13 +3,15 @@ import { DataSource } from 'typeorm';
 import InstrumentTypeORM from '../domains/instruments/Entity/Instrument.typeorm';
 import PeopleTypeORM from '../domains/people/Entity/People.typeorm';
 import ScaleTypeORM, { BandTypeORM } from '../domains/scales/Entity/Scale.typeorm';
+import * as dotenv from 'dotenv'
 
+dotenv.config();
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: parseInt(process.env.POSTGRES_USER as string) || 5432,
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'postgres',
   database: 'postgres',
   synchronize: true,
   // logging: true,

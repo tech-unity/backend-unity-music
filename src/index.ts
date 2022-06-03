1461a6cedcd3555d5eba2ce931b8c42e3d83db38
enzomoraes <enzo@DESKTOP-0U8S1NC.localdomain>
2022-06-03 10:57:32 -0400

    chore: application port

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import app from '../src/server';
+import * as dotenv from 'dotenv'
+dotenv.config();
 
-app.listen(3000, () => console.log('server is running'));
+app.listen(process.env.PORT || 3000, () => console.log('server is running'));
